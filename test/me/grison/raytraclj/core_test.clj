4a7ec5e7b9da2fef23d1e80740077a5534c3b19d
Alexandre Grison <a.grison@gmail.com>
2020-04-11 13:33:02 +0200

    init

---
+++ test/me/grison/raytraclj/core_test.clj
@@ -0,0 +1,7 @@
+(ns me.grison.raytraclj.core-test
+  (:require [clojure.test :refer :all]
+            [me.grison.raytraclj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
